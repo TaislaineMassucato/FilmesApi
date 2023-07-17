4e914de84b7b2716a1f9cca7dace829bbe3f1e97
Taislaine Massucato <massucato2@outlook.com>
2023-07-17 14:03:31 -0300

    Inclusão models, classes e um post para inserção de um filme

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
